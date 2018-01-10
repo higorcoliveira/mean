282b1c3bb8af957a8ce0ff4352bfb3e5d5926d4c
higorcoliveira <higorcoliveira@gmail.com>
2018-01-10 12:03:57 -0200

    Ajustes no build do projeto via gulp

--- frontend/gulpTasks/server.js
+++ frontend/gulpTasks/server.js
@@ -2,9 +2,9 @@ const gulp = require('gulp')
 const watch = require('gulp-watch')
 const webserver = require('gulp-webserver')
 
-//Fica escutando mudanças nos arquivos
-gulp.task('server', ['watch'], function() {
-    gulp.src('public').pipe(webserver({
+//Inicializa o webserver do gulp e chama a task que fica monitorando mudanças nos arquivos
+gulp.task('server', ['watch'], () => {
+    return gulp.src('public').pipe(webserver({
         livereload: true,
         port: 3000,
         open: true
@@ -12,7 +12,7 @@ gulp.task('server', ['watch'], function() {
 })
 
 // tasks que serão executadas caso algo seja alterado
-gulp.task('watch', function() {
+gulp.task('watch', () => {
     watch('app/**/*.html', () => gulp.start('app.html'))
     watch('app/**/*.css', () => gulp.start('app.css'))
     watch('app/**/*.js', () => gulp.start('app.js'))
