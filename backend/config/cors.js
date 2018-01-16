0b874a7f461001d81c0551f0de4dec708cb5ce70
higoroliveira <higor.oliveira@afferolab.com.br>
2018-01-16 19:03:46 -0200

    Autenticação no backend

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -4,7 +4,7 @@ fazer um filtro de CORS no banckend
 **/
 module.exports = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   next()
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const auth = require('./auth')
 
 module.exports = function(server) {
-    
-    // API routes
-   const router = express.Router();
-   server.use('/api', router);
-    
-   const billingCycleService = require('../api/billingCycle/billingCycleService');   
-   billingCycleService.register(router, '/billingCycles');
-    
-   const billingCycleSummaryService = require('../api/billingCycle/billingCycleSumaryService');
-   router.route('/billingSummary').get(billingCycleSummaryService.getSummary);
+
+	// Rotas abertas
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+	 
+	// Rotas protegidas por Token JWT	  
+	const protectedApi = express.Router()
+	server.use('/api', protectedApi)
+	protectedApi.use(auth)
+
+	const billingCycleService = require('../api/billingCycle/billingCycleService')
+	billingCycleService.register(protectedApi, '/billingCycles')
+
+	const billingCycleSummaryService = require('../api/billingCycle/billingCycleSumaryService')
+	protectedApi.route('/billingSummary').get(billingCycleSummaryService.getSummary)	
 }
